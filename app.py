6b5ad74597390154043164fa7f9355934fb96887
Your Username <your-email@example.com>
2024-11-30 13:53:18 +0700

    berhasil

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def initialize_conversation():
     # Initializing conversation memory
     memory = ConversationBufferWindowMemory()
      # Initializing GROQ chat with provided API key, model name, and settings
-    groq_chat = ChatGroq(groq_api_key=groq_api_key, model_name="mixtral-8x7b-32768",
-                         temperature=0.2)
+    groq_chat = ChatGroq(groq_api_key=groq_api_key, model_name="llama3-8b-8192",
+                         temperature=1,)
     # Creating and returning conversation chain with GROQ chat and memory
     return ConversationChain(llm=groq_chat, memory=memory)
 
@@ -26,10 +26,12 @@ conversation = initialize_conversation()
 
 # Function to handle chatbot response
 def chatbot(user_question):
-    # Getting response from conversation chain based on user question
-    response = conversation(user_question)
-    # Returning the response part of the conversation output
-    return response['response']
+    try:
+        response = conversation(user_question)
+        return response['response']
+    except Exception as e:
+        return f"Terjadi error: {e}"
+
 
 # Creating Gradio interface with chatbot function as backend
 iface = gr.Interface(fn=chatbot, inputs="textbox", outputs="textbox", title="Groq Chat App", description="Ask a question and get a response.")
